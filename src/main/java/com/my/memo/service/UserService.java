449f584782b8adfcb93474fff823b52520fdb19d
ㅇi쀼z1 <yaewon0411@naver.com>
2024-10-03 20:01:47 +0900

    [수정] javadoc 추가

--- src/main/java/com/my/memo/service/UserService.java
+++ src/main/java/com/my/memo/service/UserService.java
@@ -20,7 +20,12 @@ import java.util.*;
 
 import static com.my.memo.dto.user.ReqDto.*;
 import static com.my.memo.dto.user.RespDto.*;
-
+/**
+ * 유저 관련 서비스 클래스입니다
+ *
+ * 로그인, 로그아웃, 회원가입, 정보 조회, 정보 수정, 유저 삭제와 관련된 비즈니스 로직을 처리합니다
+ * 트랜잭션 관리와 예외 처리를 직접 처리합니다
+ */
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -30,7 +35,12 @@ public class UserService {
     private final DataSource dataSource;
     private static final Logger log = LoggerFactory.getLogger(UserService.class);
 
-    //로그아웃
+    /**
+     * 현재 사용자를 로그아웃 처리합니다
+     *
+     * @param session 현재 사용자의 세션
+     * @return 로그아웃 성공 여부 및 유저 ID를 포함한 응답 DTO
+     */
     public UserLogoutRespDto logout(HttpSession session){
         Long userId = (Long) session.getAttribute("userId");
         session.invalidate();
@@ -39,7 +49,14 @@ public class UserService {
     }
 
 
-    //유저 삭제
+    /**
+     * 현재 사용자의 계정을 삭제합니다
+     *
+     * 유저와 관련된 스케줄도 함께 삭제되며, 예외 발생 시 롤백됩니다
+     *
+     * @param session 현재 사용자의 세션
+     * @return 삭제 작업의 성공 여부 및 유저 ID를 포함한 응답 DTO
+     */
     public UserDeleteRespDto deleteUser(HttpSession session) {
         //유저 꺼내기
         Long userId = (Long) session.getAttribute("userId");
@@ -88,7 +105,12 @@ public class UserService {
 
 
 
-    //유저 정보 조회
+    /**
+     * 현재 사용자의 정보를 조회합니다
+     *
+     * @param session 현재 사용자의 세션
+     * @return 유저 정보를 포함한 응답 DTO
+     */
     public UserRespDto getUserInfo(HttpSession session){
         //유저 꺼내기
         Long userId = (Long) session.getAttribute("userId");
@@ -123,7 +145,16 @@ public class UserService {
     }
 
 
-    //유저 정보 수정
+    /**
+     * 현재 사용자의 정보를 수정합니다
+     *
+     * 이메일 수정 시 중복 체크 후, 유저 정보를 업데이트하고 트랜잭션을 통해 처리합니다
+     * 예외 발생 시 트랜잭션 롤백합니다
+     *
+     * @param userModifyReqDto 수정할 유저 정보를 담은 DTO
+     * @param session 현재 사용자의 세션
+     * @return 수정된 유저 정보를 포함한 응답 DTO
+     */
     public UserModifyRespDto updateUser(UserModifyReqDto userModifyReqDto, HttpSession session){
 
         //유저 꺼내기
@@ -181,7 +212,15 @@ public class UserService {
     }
 
 
-    //회원가입
+    /**
+     * 회원가입을 처리합니다
+     *
+     * 이메일 중복을 확인한 후, 새로운 유저를 저장합니다
+     * 트랜잭션을 통해 처리되며, 예외 발생 시 롤백됩니다
+     *
+     * @param joinReqDto 회원가입 정보를 담은 DTO
+     * @return 생성된 유저 정보를 포함한 응답 DTO
+     */
     public JoinRespDto join(JoinReqDto joinReqDto){
 
         Connection connection = null;
@@ -226,7 +265,16 @@ public class UserService {
         }
     }
 
-    //로그인
+    /**
+     * 로그인 처리를 수행합니다
+     *
+     * 이전 세션을 무효화하고 새로운 세션을 생성한 후, 유저 정보를 세션에 저장합니다
+     * 이메일과 비밀번호를 검증한 후 유저 정보를 반환합니다
+     *
+     * @param loginReqDto 로그인 요청 정보를 담은 DTO
+     * @param request HttpServletRequest 객체로 세션을 관리
+     * @return 로그인된 유저 정보를 포함한 응답 DTO
+     */
     public LoginRespDto login(LoginReqDto loginReqDto, HttpServletRequest request){
 
         HttpSession session = request.getSession(false);
