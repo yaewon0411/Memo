b67a1a6c9058c90d00cbdbef0cbc823c647077ed
ㅇi쀼z1 <yaewon0411@naver.com>
2024-10-25 17:44:59 +0900

    [수정] ErrorCode를 사용하도록 api 예외 수정

--- src/main/java/com/my/memo/client/weather/WeatherClient.java
+++ src/main/java/com/my/memo/client/weather/WeatherClient.java
@@ -2,10 +2,10 @@ package com.my.memo.client.weather;
 
 import com.my.memo.client.weather.dto.DateWeatherDto;
 import com.my.memo.ex.CustomApiException;
+import com.my.memo.ex.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -26,7 +26,7 @@ public class WeatherClient {
 
         } catch (Exception e) {
             log.error("날씨 정보 조회 중 오류 발생: " + e.getMessage());
-            throw new CustomApiException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "날씨 정보 조회 중 오류 발생");
+            throw new CustomApiException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
     }
 
